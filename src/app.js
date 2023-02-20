9322e5abbe72e56d1b3e708c096ea4da3a88f870
edwinsuesca <edwinsuesca@vivaldi.net>
2023-02-20 17:15:50 -0500

    [updated] Depurados archivos app.js y controllers.js.

--- src/app.js
+++ src/app.js
@@ -3,25 +3,7 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 
-// middlewares
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
-app.use(cors({
-  origin: '*'
-}));
-
 const controllers = require('./controllers');
-
-/* const whitelist = ['http://localhost:3400']
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error())
-    }
-  }
-} */
 
 // middlewares
 app.use(express.urlencoded({extended: false}));
--- src/controllers.js
+++ src/controllers.js
@@ -3,18 +3,22 @@ const token = process.env.TOKEN_BOT;
 const chatId = process.env.CHAT_ID;
 const bot = new TelegramBot(token, { polling: true, parse_mode: 'HTML' });
 
-const sendMessage = async (req, res) => {
-    const { name, message, media } = req.body;
-    if(!name || !message || !media) return res.status(400).send('Bad request');
-
-    const messageToSend = `<b>Nombre: </b>${name}\n<b>Medio: </b>${media}\n\n<b>Mensaje: </b>\n<i>${message}</i>`
-    bot.sendMessage(chatId, messageToSend, { parse_mode: 'HTML' })
-    .then(() => {
-        res.status(200).send('Mensaje enviado correctamente.');
-    })
-    .catch((error) => {
-        res.status(500).send('Error al enviar el mensaje:', error);
-    });
+const sendMessage =  (req, res) => {
+    try{
+        const { name, message, media } = req.body;
+        if(!name || !message || !media) return res.status(400).send('Bad request');
+    
+        const messageToSend = `<b>Mensaje: </b>\n<i>${message}</i>\n\n<b>Enviado por: </b>${name}\n<b>Medio: </b>${media}`
+        bot.sendMessage(chatId, messageToSend, { parse_mode: 'HTML' })
+        .then(() => {
+            res.status(200).json({message: 'Mensaje enviado correctamente.'});
+        })
+        .catch((error) => {
+            res.status(500).json({message: 'Error al enviar el mensaje:', error: error});
+        });
+    } catch (err){
+        res.status(500).send('Internal server error');
+    }
 };
 
 module.exports = {
