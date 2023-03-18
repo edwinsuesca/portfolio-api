7db80940d96e934d761a2f64a041fd591d25aa6a
Edwin Suesca <edwinsuesca@vivaldi.net>
2023-03-18 16:26:21 -0500

    [updated] Ajustado inicio del servidor para producción

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const app = require('./src/app')
 const port = process.env.PORT || 3200;
 
 //INICIAR SERVIDOR EN ENTORNO DE DESARROLLO
-app.listen(port, () => {
+/* app.listen(port, () => {
     console.log(`Servidor web en ejecución en http://localhost:${port}`);
-});
+}); */
 
 //INICIAR SERVIDOR EN ENTORNO DE DESARROLLO
 
 // Configura opciones de SSL/TLS
-/* const options = {
+const options = {
   key: fs.readFileSync('/etc/letsencrypt/live/edwinsuesca.net/privkey.pem'),
   cert: fs.readFileSync('/etc/letsencrypt/live/edwinsuesca.net/cert.pem')
 };
@@ -21,4 +21,4 @@ app.listen(port, () => {
 // Crea un servidor HTTPS utilizando Express
 https.createServer(options, app).listen(port, () => {
   console.log(`Servidor HTTPS escuchando en puerto ${port}`);
-}); */
+});
